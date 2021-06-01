59c80b981b715dc906585aa982edc09f380639dc
Shubham Agarwal <>
2021-06-01 12:43:33 +0530

    Add code for blob storage

---
+++ src/azureBlob.js
@@ -0,0 +1,65 @@
+// THIS IS SAMPLE CODE ONLY - NOT MEANT FOR PRODUCTION USE
+import { BlobServiceClient} from '@azure/storage-blob';
+
+const sasToken = process.env.storagesastoken || "sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-05-28T16:49:40Z&st=2021-05-24T08:49:40Z&spr=https&sig=Ce0EinaxClvGkB71InL%2B2IEjAfd0gMsiqnNfo9wCRr8%3D"; // Fill string with your SAS token
+const containerName = `moduleimports`;
+const storageAccountName = process.env.storageresourcename || "storagename"; // Fill string with your Storage resource name
+
+// Feature flag - disable storage feature to app if not configured
+export const isStorageConfigured = () => {
+  return !((!storageAccountName || !sasToken));
+};
+
+// return list of blobs in container to display
+const getBlobsInContainer = async (containerClient) => {
+  const returnedBlobUrls = [];
+
+  // get list of blobs in container
+  // eslint-disable-next-line
+  for await (const blob of containerClient.listBlobsFlat()) {
+    console.log('blob',blob);
+    console.log('containerClient.listBlobsFlat()',containerClient.listBlobsFlat());
+    // if image is public, just construct URL
+    returnedBlobUrls.push(
+      `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
+    );
+  }
+
+  return returnedBlobUrls;
+};
+
+
+const createBlobInContainer = async (containerClient, file) => {
+  
+  // create blobClient for container
+  const blobClient = containerClient.getBlockBlobClient(file.name);
+
+  // set mimetype as determined from browser with file upload control
+  const options = { blobHTTPHeaders: { blobContentType: file.type } };
+
+  // upload file
+  await blobClient.uploadBrowserData(file, options);
+  await blobClient.setMetadata({UserName : 'shubham'});
+};
+
+const uploadFileToBlob = async (file) => {
+  if (!file) return [];
+
+  // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
+  const blobService = new BlobServiceClient(
+    `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
+  );
+    console.log('blobService', blobService);
+  // get Container - full public read access
+  const containerClient = blobService.getContainerClient(containerName);
+
+  // upload file
+  await createBlobInContainer(containerClient, file);
+
+  // get list of blobs in container
+  return getBlobsInContainer(containerClient);
+};
+// </snippet_uploadFileToBlob>
+
+export default uploadFileToBlob;
+
